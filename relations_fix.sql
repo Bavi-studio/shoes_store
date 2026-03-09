5ab26d062a43fc3128ed04a6b79301be885e8cb3
Bavi <vasilevandrei888@gmail.com>
2026-03-09 13:10:46 +0300

    Delete relations_fix.sql

--- relations_fix.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE public.products
-ADD COLUMN supplier_id INTEGER;
-
-ALTER TABLE public.products
-ADD CONSTRAINT fk_products_suppliers
-FOREIGN KEY (supplier_id)
-REFERENCES public.suppliers (id);
-
